2945604e70f3c1fe06e8030fdbe721978021d386
Lucía <lusanmanso@gmail.com>
2026-03-08 15:43:10 +0100

    P1: density and last_event

--- practica1/WikipediaAnalytics.py
+++ practica1/WikipediaAnalytics.py
@@ -1,80 +1,104 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import regex as re
-from pathlib import Path
 
 
 class WikipediaAnalytics:
-    def __init__(self, list_of_strings):
-        """
-        Constructor: guarda la lista de strings.
-        list_of_strings puede ser rutas a HTML, URLs, nombres, etc.
-        """
+   def __init__(self, list_of_strings):
+      """
+      Constructor: guarda la lista de strings.
+      list_of_strings puede ser rutas a HTML, URLs, nombres, etc.
+      """
+      self.sources = list_of_strings
+      self.df = None  # Se inicializará en scrap()
 
-        if not isinstance(list_of_strings, (list, tuple)):
-            raise ValueError("list_of_strings debe ser una lista o tuple de strings.")
+   def scrap(self):
 
-        if not all(isinstance(item, str) for item in list_of_strings):
-            raise ValueError(
-                "Todos los elementos de list_of_strings deben ser strings."
-            )
+      # 1. Abrir el archivo HTML local y parsearlo con bs4 para convertirlo en un objeto navegable
+      for route in self.sources:
+         with open(route, 'r', encoding='utf-8') as file:
+            content = file.read()
 
-        self.sources = list_of_strings
-        self.df = None  # Se inicializará en scrap()
+         # Crear objeto beautifulsoup
+         soup = BeautifulSoup(content, 'html.parser')
 
-    def _read_html(self, source: str) -> str:
-        p = Path(source)
-        return p.read_text()
+         # Buscar la tabla que en wikipedia es siempre una infobox
+         table = soup.find('table', {'class': 'infobox'})
 
-    def scrap(self):
-        """
-        Inicializa el DataFrame.
-        Aquí deberías procesar tus archivos HTML y llenar self.df.
-        En este ejemplo se deja un df básico como placeholder.
-        """
+         country_name, area, water, population, density, GPD, last_event, latitude, longitude = "", "", "", "", "", "", "", "", ""
 
-        """
-        - self.df siempre existe al terminar y tiene mismas columnas en el mismo orden + cada fila representa un país
-        """
-        target_columns = [
-            "Country Name",
-            "Latitude (º)",
-            "Longitude(º)",
-            "Area (KM^2)",
-            "Water (%)",
-            "Population (hab.)",
-            "Density (hab./km^2)",
-            "GDP ($)",
-            "Last Event Date",
-        ]
+         for row in table.find_all('tr'):
+            header = row.find('th')
+            data = row.find('td')
 
-        records = []
+            # verificar que tienen datos
+            if header and data:
+               header_text = header.get_text(strip=True).lower()
 
-        self.df = pd.DataFrame.from_records(records, columns=target_columns)
-        self.df["Last Event Date"] = pd.to_datetime(
-            self.df["Last Event Date"], errors="coerce"
-        )
+               # Buscar coincidencias
+               if 'cabecera' in header_text:
+                  country_name = data.get_text(strip=True)
+               elif 'superficie' in header_text:
+                  area = data.get_text(strip=True)
+               elif 'agua' in header_text:
+                  water = data.get_text(strip=True)
+               elif 'poblacion total' in header_text:
+                  population = data.get_text(strip=True)
+               elif 'densidad' in header_text:
+                  density = data.get_text(strip=True)
+               elif 'pib' in header_text:
+                  GPD = data.get_text(strip=True)
+               elif 'formación' in header_text:
+                  last_event = data.get_text(strip=True)
+               elif 'coordenadas' in header_text:
+                  latitude, longitude = data.find('span', class_='latitud').get_text(strip=True), data.find('span', class_='longitude').get_text(strip=True)
 
-        return self
+         # dicc para comprobacion
+         raw_data = {
+            'country_name': country_name,
+            'area': area,
+            'water': water,
+            'population': population,
+            'density': density,
+            'GPD': GPD,
+            'last_event': last_event,
+            'latitude': latitude,
+            'longitude': longitude
+         }
 
-    def select_row_by_value(self, col_name, value):
-        """
-        Devuelve la fila (o filas) cuyo valor en col_name coincide con value.
-        """
-        pass
 
-    def get_columns(self, col_names):
-        """
-        Recibe una columna o lista de columnas y devuelve esa parte del DataFrame.
-        """
-        pass
+         # print de comprobacion para espania
+         if 'espania' in route.lower():
+            print(f"Comprobacion de datos {route}")
+            for key, value in raw_data.items():
+               print(f"{key}: {value}")
+            print("fin")
 
-    def aggregate_column(self, col_name, operation):
-        """
-        Agrega una columna numérica según una operación dada:
-        - 'max'
-        - 'min'
-        - 'mean'
-        Devuelve el resultado como un float.
-        """
-        pass
+         pass
+
+   def select_row_by_value(self, col_name, value):
+      """
+      Devuelve la fila (o filas) cuyo valor en col_name coincide con value.
+      """
+      pass
+
+   def get_columns(self, col_names):
+      """
+      Recibe una columna o lista de columnas y devuelve esa parte del DataFrame.
+      """
+      pass
+
+   def aggregate_column(self, col_name, operation):
+      """
+      Agrega una columna numérica según una operación dada:
+      - 'max'
+      - 'min'
+      - 'mean'
+      Devuelve el resultado como un float.
+      """
+      pass
+
+if __name__ == "__main__":
+   files = ["files/espania_es.html"]
+   scraper = WikipediaAnalytics(files)
+   scraper.scrap()
